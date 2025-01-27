eab2d8ea17416795a16e6fba92ca04912868ca86
akashkewat99 <akashkewat99@gmail.com>
2025-01-28 01:24:31 +0530

    updated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const app = express();
 const connection = require("./config/db");
 const userRoutes = require("./Routes/user.routes");
 const todoRoutes = require("./Routes/todo.routes");
@@ -7,8 +8,21 @@ const cors = require("cors");
 
 const PORT = process.env.PORT;
 
-const app = express();
-app.use(cors());
+// let's tackle cors
+const corsOptions = {
+  // origin: "http://localhost:5173",
+  origin: (origin, callback) => {
+    // Check if the origin is allowed
+    const allowedOrigins = ["http://localhost:5173"];
+    const isAllowed = allowedOrigins.includes(origin);
+    callback(null, isAllowed ? origin : false);
+  },
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use("api/users", userRoutes);
