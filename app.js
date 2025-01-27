049eccda01795992048df08d5f3b2b8d60ab6258
akashkewat99 <akashkewat99@gmail.com>
2025-01-27 23:54:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const connection = require("./config/db");
+const userRoutes = require("./Routes/user.routes");
+const todoRoutes = require("./Routes/todo.routes");
+const cors = require("cors");
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/todos", todoRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello Express" });
+});
+
+app.listen(PORT, async () => {
+  try {
+    await connection(process.env.MONGO_URI);
+    console.log(`Database is connected`);
+    console.log(`⚙️  Server is listening at http://localhost:${PORT}`);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
