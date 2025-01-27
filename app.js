d82e9fcceb085606f539b1a50abc3b33102e5c33
akashkewat99 <akashkewat99@gmail.com>
2025-01-28 00:59:19 +0530

    updated

--- app.js
+++ app.js
@@ -8,15 +8,25 @@ const cors = require("cors");
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(
-  cors({
-    origin: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
 app.use(express.json());
+// Add logging middleware to see incoming requests
+app.use((req, res, next) => {
+  console.log('Incoming request:', req.method, req.path);
+  console.log('Origin:', req.headers.origin);
+  next();
+});
+
+app.use(cors({
+  origin: 'http://localhost:5173',  
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders:  ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
+
+app.options('*', cors()) 
+
+
 
 app.use("/users", userRoutes);
 app.use("/todos", todoRoutes);
